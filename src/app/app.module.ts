1d3fea314210c384041bb673980ba7b417b55321
wei.zeng <wei.zeng@ctitv.com.tw>
2023-02-22 18:46:28 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+import { ContactComponent } from './contact/contact.component';
+import { Route, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ErrorComponent } from './error/error.component';
+
+const appRoute: Routes = [
+  // {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'courses/:id', component: CoursesComponent},
+  {path: '**', component: ErrorComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    CoursesComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
